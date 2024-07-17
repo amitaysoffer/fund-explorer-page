3897afdb6c8e4240e6aefbbbcbbabbe6c801547d
amitaysoffer <amitay@spring.co.uk>
2024-07-17 23:17:49 +0100

    adds mock to test

--- tests/components/ListView.test.tsx
+++ tests/components/ListView.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import "@testing-library/jest-dom";
 import data from "../../src/data.json";
 import ListView from "../../src/components/ListView";
 
 describe("ListView component", () => {
   it("should render all funds with no filters", () => {
-    render(<ListView funds={data} />);
+    const handleSortClick = vi.fn();
+    render(<ListView funds={data} handleSortClick={handleSortClick} />);
 
     const funds = screen.getAllByTestId("fund");
 
